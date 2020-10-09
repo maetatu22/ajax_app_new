16fbc3f4d5170a74f41b1a136966c26ae508fe6b
maetatu22 <zbn.ex.zz@gmail.com>
2020-10-09 15:50:59 +0900

    Ajax準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,18 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.all.order(id: "DESC")
+    @post = Post.new
   end
 
-  def new
-  end
-
+ 
   def create
-    Post.create(content: params[:content])
+    Post.create(post_params)
+    redirect_to action: :index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end
